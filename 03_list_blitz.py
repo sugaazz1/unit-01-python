72c612453f85de401462f9a0acc5e0fc57c3b271
Caleb Ogboe <cogboe@brooklynsteamcenter.org>
2025-09-15 10:51:31 -0400

    ezzz

---
+++ 03_list_blitz.py
@@ -0,0 +1,82 @@
+"""
+Task 1: Create a list
+Create a list with 4 elements and print it.
+"""
+print("---Task 1---")
+
+best_foods = ["Jollof rice", "Fried Rice", "Rice and Stew", "Meat Pie"]
+
+print(best_foods)
+
+"""
+Task 2: Add Element to a List
+Add an element to the end of the list. Print the updated 
+list.
+"""
+print("---Task 2---")
+
+best_foods.append("Akara")
+
+print(best_foods)
+
+"""
+Task 3: Remove Element from a List
+Remove a specific element from the list. Print the 
+updated list.
+"""
+print("---Task 3---")
+
+best_foods.remove("Jollof rice")
+
+print(best_foods)
+
+"""
+Task 4: Modify Element in a List
+Modify an element at a specific index with a new value. 
+Print the updated list.
+"""
+print("---Task 4---")
+
+
+best_foods[2] = "Fufu"
+
+print(best_foods)
+
+"""
+Task 5: Append Multiple Elements to a List
+Append multiple elements to the end of the list. Print 
+the updated list.
+"""
+print("---Task 5---")
+
+best_foods.append("Egusi soup")
+
+
+
+
+"""
+Task 6: Delete Element at a Specific Index
+Delete an element at a specific index. Print the updated 
+list.
+"""
+print("---Task 6---")
+
+
+
+
+"""
+Task 7: Slicing lists
+Create a new variable equal to the first 2 items of your list
+Then print out the new variable
+"""
+print("---Task 7---")
+
+
+
+
+"""
+Task 8: Extend a List
+Extend the list with the elements of another list. Print 
+the updated list.
+"""
+print("---Task 8---")
