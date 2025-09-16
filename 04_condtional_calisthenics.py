02dfa30f2de08a233eb1c249ab8ce3571073b9bd
Caleb Ogboe <cogboe@brooklynsteamcenter.org>
2025-09-16 10:52:41 -0400

    even

--- 04_condtional_calisthenics.py
+++ 04_condtional_calisthenics.py
@@ -3,8 +3,13 @@ Exercise 1:
 Check if an integer is even and greater than 10.
 Return True if both conditions are met, False otherwise.
 '''
+print("--Task 1---")
 
-
+even_greater = int(input("Insert an interger: "))
+if even_greater > 10 and even_greater % 2==0:
+    print("True") 
+else:
+    print("False")
 
 
 '''
@@ -12,6 +17,8 @@ Exercise 2:
 Determine the ticket price based on age and student status.
 Price is $10 if under 18 or a student, $20 otherwise.
 '''
+print("--Task 2---")
+
 
 
 '''
@@ -20,12 +27,14 @@ Prompt the user to enter a fruit name. Check if the fruit is in the list.
 If it is, print "Yes, that fruit is in the list." 
 If it's not, print "No, that fruit is not in the list."
 '''
-
+print("--Task 3---")
 
 '''
 Exercise 4:
 Check if a year is a century year and a leap year.
 '''
+print("--Task 4---")
+
 
 '''
 Exercise 5:
@@ -33,9 +42,12 @@ Calculate the cost of shipping for an online order based on the order weight and
 The shipping cost is $5 per kilogram for Zone A and $7 per kilogram for Zone B. 
 If the order weight is less than 0 kg, return an error message.
 '''
+print("--Task 5---")
+
 
 '''
 Exercise 6:
 Determine the type of a triangle based on side lengths.
 Equilateral, Isosceles, Scalene, or Not a triangle.
 '''
+print("--Task 6---")
