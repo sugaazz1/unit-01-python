57a2f6b47cf4112ff1bada9343b5477bd4ae5e8d
Mr. Sussy <cogboe@brooklynsteamcenter.org>
2025-09-22 21:32:31 -0400

    latenight

--- 06_whimsical_whiles.py
+++ 06_whimsical_whiles.py
@@ -8,6 +8,7 @@ i = 1
 while i < 11:
     print(i)
     if i == 10:
+#The loops breaks when i==10. As i originally equals 1, the code i += 1 adds one to the original i everytime the code loops, it keeps adding one until the conditions (if i ==10) is met, then it breaks.
         break
     i += 1
 
@@ -18,7 +19,7 @@ Write a program that uses a while loop to print numbers from 10 to 1 in descendi
 """
 print("---Task 2---")
 i = 11
-
+#Similar scenaria here, but instead the loop breaks by itself when i is less than 1. If not, the loop will just keep subtracting into the negatives.
 while i > 1:
     i -= 1
     print(i)
@@ -30,11 +31,11 @@ Write a program that calculates the factorial of a given number using a while lo
 """
 print("---Task 3---")
 
-fact_input = int(input("Enter a number for the factorial result:"))
+fact_input = int(input("Enter a number for the factorial result: "))
+#the fact_input variable stores the input for the factorial.
 result = 1 
 
 while fact_input > 0:
-    #print(fact_input)
     result = fact_input * result
     fact_input -= 1
 print(result)
@@ -50,10 +51,11 @@ print("---Task 4---")
 
 passd = "hotdogs"
 pass1 = input("Enter the password:")
-
+#first create a variable to store the password and the input.
 while pass1 != passd:
     print("Incorrect password, try again")
     pass1 = input("Enter the password:")
+#I put the variable for the input so that when the user tries to guess the password and they fail, it'll allow them to try again.
 else:
     print("Correct Password")
 
@@ -66,10 +68,30 @@ Write a program that calculates the sum of the digits of a given number using a 
 """
 print("---Task 5---")
 
-#number = int(input("Inser a number:"))
-
 """
 6. Fibonacci Series:
 Write a program that prints the first n numbers in the Fibonacci sequence using a while loop.
 """
 print("---Task 6---")
+
+n = int(input("Enter the numbe of Fibonacci sequence you would like to print: "))
+#I used n as the variable to match the instructions.
+begin = 0
+
+fst_fibnum = 0
+snd_fibnum = 1
+#The first fibonaccie sequence starts with a 0 then a 1 as it adds the previous results to the new one.
+
+while n > 0: 
+    print(fst_fibnum)
+    
+    result = fst_fibnum + snd_fibnum
+    
+    fst_fibnum = snd_fibnum
+    snd_fibnum = result
+#This 2 fst_fibnum and snd_fibnum basically changes the roles of it. For example once the previous number is stalled, it's then added to the second number by these codes, fst_fibnum = snd_fibnum, snd_fibnum = result so that the previous result is added to the next one.
+    snd_fibnum = result 
+    n-=1
+else:
+    print("Enter a vaild number: ")
+#if a valid number isn't inputed, then it tells them that.
