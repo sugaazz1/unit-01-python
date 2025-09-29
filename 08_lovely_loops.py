fef36c112cb042f82992f7c8c7b4b0fdcda52124
Caleb Ogboe <cogboe@brooklynsteamcenter.org>
2025-09-29 10:24:04 -0400

    lovely_bruh

--- 08_lovely_loops.py
+++ 08_lovely_loops.py
@@ -8,57 +8,56 @@ security_level = input("Enter security level. Low, Medium or High: ")
 attempts = 0
 login_successful = False
 
-if security_level.lower() == "low":
-    password = "password"
-    password_input = input("Guess the correct password: ")
-    while password_input != password:
-        attempts += 1
-        password_input = input("Incorrect, guess the correct password: ")
-        if attempts == lockout_threshold:
-#This code basically shows that if it's on their last try and they get it right, it'll let them know that they actually got it.
-            if password_input == password:
-                print("Lowkey Easy for real.")
-            else:
-                print("You've reached the maximum attempt.")
-            break
-    else:
-#If they guess the password on the first try, this message pops up.
-        print("Good job, you got it.")
+while attempts < lockout_threshold:
 
-elif security_level.lower() == "medium":
-    password = "dictionaries"
-    password_input = input("Guess the correct password: ")
-    while password_input != password:
-        attempts += 1
-        password_input = input("Incorrect, guess the correct password: ")
-        if attempts == lockout_threshold:
-            if password_input == password:
-                print("Nice you got on the very last try.")
-            else:
-#If the user gets to hte last attepmt and still didn't guess the password. I'll print the following statement below.
-                print("You've reached the maximum attempt.")
-            break
-    else:
-        print("You got it.")
+    if security_level.lower() == "low":
+        password_input = input("Guess the correct password: ")
+        if password_input.lower() == "password":
+            login_successful = True
+            print("Login Successful!")
+    #If they guess the password on the first try, this message pops up.
 
-elif security_level.lower() == "high":
-    password = "supercalifragilisticexpialidocious"
-    password_input = input("Guess the correct password: ")
-    while password_input != password:
-        attempts += 1
-        password_input = input("Incorrect, guess the correct password: ")
-#Putting the password input again serves to make sure that when the user guesses the password wrong, it'll give them the option to try and guess the password again.
-        if attempts == lockout_threshold:
-            if password_input == password:
-                print(f"Crazy you guessed this {username}??")
-            else:
-                print("You've reached the maximum of attempts.")
             break
-    else: 
-#I put the format option to show the user that they're recognized for actually getting a word like this.
+        else:
+            attempts += 1
+            print("Incorrect password, Try again.")
+            if attempts >= lockout_threshold:
+                print("You've reached the maximum amount of attempt.")
+                break
+
+            
+
+    elif security_level.lower() == "medium":
+        password_input = input("Guess the correct password: ")
+        if password_input.lower() == "dictionaries":
+            login_successful = True
+            print("Nicee you got it.")
+            break
+        else: 
+            attempts += 1
+            print("Incorrect password, try again.")
+            if attempts >= lockout_threshold:
+    #If the user gets to hte last attepmt and still didn't guess the password. I'll print the following statement below.
+                    print("You've reached the maximum attempt.")
+                    break
+
+    elif security_level.lower() == "high":
+        password_input = input("Guess the correct password: ")
+        if password_input.lower() == "supercalifragilisticexpialidocious":
+            login_successful = True
             print(f"Wow you actually got it {username} props to you nerdd.")
+#I put the format option to show the user that they're recognized for actually getting a word like this.
+            break
+        else:
+             attempts += 1 
+             print("Incorrect password, try again.")
+             if attempts >= lockout_threshold:
+                    print("You've reached the maximum of attempts.")
+                    break
+
+    #I put the format option to show the user that they're recognized for actually getting a word like this.
 
 
-else:
-   print("Enter a valid Security Level.")
-#if the user enters anything other than the security level. It'll print the following statement.
+    else:
+        print("Enter a valid Security Level.")
+    #if the user enters anything other than the security level. It'll print the following statement.
