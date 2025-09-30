75c8c8285ffc771b9e91bc29fdf55c8ea62f9815
Caleb Ogboe <cogboe@brooklynsteamcenter.org>
2025-09-30 10:50:43 -0400

    to_doo

---
+++ to_do.py
@@ -0,0 +1,24 @@
+print("Welcom to your To Do list. ")
+print()
+my_list = []
+while True:
+    ins_todo = input("Would you like to add or remove a to do: ")
+    if ins_todo.lower() == "add":
+        todo_list = input("What would you like to add: ")
+        print()
+        my_list.append(todo_list)
+        print()
+        print("Your current to do list:")
+        for i in my_list:
+             print(i)
+
+    elif ins_todo.lower() == "remove":
+            todo_rem = int(input("What # to do would you like to remove: "))
+            print()
+            del my_list[todo_rem -1]
+            print()
+            print("Your current to do list:")
+            for i in my_list:
+                 print(i)
+    else:
+         print("Enter a valid input.")
