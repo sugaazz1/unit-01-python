1ef896ab549fb78cd10d7c111296dedf3c07114a
Mr. Sussy <cogboe@brooklynsteamcenter.org>
2025-10-03 20:00:14 -0400

    3rd_debug

--- 08_debugging.py
+++ 08_debugging.py
@@ -1,14 +1,3 @@
-"""temperature = 75
-
-if temperature > 80:
-    print("It's hot")
-elif temperature > 50:
-    print("It's temperate")
-else:
-    print("It's cold")
-
-    """
-
 """text = input("Hello, world, my name is ")
 count = 0
 
@@ -19,7 +8,7 @@ for char in text:
 print(count)
 """
 
-print("give me a number")
+"""print("give me a number")
 n = int(input())
 
 for num in range(1, n):
@@ -27,16 +16,34 @@ for num in range(1, n):
         print(num, "is even.")
     else:
         print(num, "is odd.")
+"""
 
-
-num = int(input("Enter an integer: "))
+"""num = int(input("Enter an integer: "))
 
 if num < -1:
   print("No negative numbers.")
 else:
   result = 1
-  for i in range(1, num):
-    result *= i
-       
+  for i in range(-1, num):
+    result *= num
 
   print("Factorial of " + num + "is" + result)
+  """
+
+"""attempts = 0
+correct_password = "secret" 
+
+
+while True:
+    password = input("Enter your password: ")
+    #attempts += 1
+    if password.lower() == "incorrect_password":
+        print("Correct password!")
+        break
+    else:
+        attempts += 1
+        print("Incorrect password")
+        if attempts == 3:
+            print("Too many attempts")
+            break
+"""
