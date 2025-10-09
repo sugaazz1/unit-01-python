bd2c336f59e6ca6537f3e8327d791a3544ec41df
Caleb Ogboe <cogboe@brooklynsteamcenter.org>
2025-10-09 09:05:25 -0400

    brackets_list

--- 11_random.py
+++ 11_random.py
@@ -6,7 +6,7 @@ Write a program that simulates rolling a six-sided die 10 times.
 Print each roll result.
 """
 print("---Task 1---")
-dice = random.randrange(1,7)
+dice = [random.randrange(1,7) for x in range(10)]
 #the range is 1,7 because 1 is inclusive, while 7 is exclusive. If it was 1,6, then it'll only count up to 5 not 6.
 print(dice)
 
@@ -21,7 +21,7 @@ print("---Task 2---")
 flts = [random.uniform(0.0, 1.0) for x in range(5)]
 #The .uniform code works with the random module by generating random float withing the given range. randrange and randint only works with integers.
 print(flts)
-
+#The brackets in this code means that I am creating a list of 5 floating numbers, between the given numbers.
 flts2 = [random.uniform(10.0, 20.0) for x in range(5)]
 print(flts2)
 
