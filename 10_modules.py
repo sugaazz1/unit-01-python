ac353a65ba25476de9399a21d793a8bc02a26ac7
Mr. Sussy <cogboe@brooklynsteamcenter.org>
2025-10-08 21:53:50 -0400

    latenight2

--- 10_modules.py
+++ 10_modules.py
@@ -6,15 +6,20 @@ Task 1 (os module):
 Write a Python program that prints the current folder (working directory) using the os module.
 """
 something_som = os.getcwd()
+#To get the name of the current folder/directory  you are using, using an os module, you use os.getcwd()
 print(something_som)
 """
 Task 2 (os module):
 Create a new directory called "test_folder" in the current directory.
 Then print a list of all files and directories in the current directory.
 """
-#test_folder = "hehe hope it works"
-#os.mkdir(test_folder)
-#print("Folders in directories", os.listdir(something_som))
+test_folder = "hehe hope it works"
+#This is the variable that holds the name of the folder.
+os.mkdir(test_folder)
+#The os.mkdir() is a python code used to create folders/ directories. And it's going to print what the variable is equals to.
+print("Folders in directories", os.listdir(something_som))
+
+
 
 #os.mkdir("test_folder")
 #folder = os.listdir()
@@ -29,13 +34,14 @@ working directory. If it doesn't exist, create it. If it does exist, print
 """
 if os.path.isdir("data") == True:
     print("Directory exists")
+#I used conditional statement to check if a folder called data already existes. If so print that it exists.
 else: 
     print("Directory does not exists.")
     creat = input("create directory? ")
     if creat == "yes":
         os.mkdir("data")
         print("Directory created")
-
+#if the folder doesn't exist, it tells the user. Then it asks the user if they want to create the directory.
 
 """
 Task 4 (os.path module):
@@ -45,8 +51,9 @@ If it exists, print its path. If it doesn't exist, print "File not found."
 os.getcwd()
 print(os.getcwd())
 os.listdir()
-if not os.path.exists("config.txt"):
+if not os.path.exists("config.txt"):#i used the .exists with path to check if it exists or not.
     print("File not found")
+#This code basically just checks if a certain file is in the directory, if it is, it'll say found, if not, it'll say not found.
 else:
     print("File found.")
 
@@ -55,13 +62,18 @@ Task 5 (sys module):
 Write a program that prints the Python version you are currently using.
 """
 print(sys.version)
-
+#The sys.version is the code that tells you what version of python you are currently using. sys = system
 """
 Task 6 (sys module):
 Write a program that prints the platform your Python interpreter is running on
 (e.g., 'linux', 'win32', 'darwin'). The output should be user friendly names
 "Linux", "Windows", "MacOS"
 """
-
+#sys.platform tells the reader what operating system interpreter is running on. 
 if sys.platform.lower() == "darwin":
     print("macOS")
+elif sys.platform.lower() == "win32":
+    print("Windows")
+elif sys.platform.lower() == "linux":
+    print("Linux")
+#These other sys.platform checks if python is running on other different operating systems.
--- 11_random.py
+++ 11_random.py
@@ -5,7 +5,9 @@ Task 1 (random module):
 Write a program that simulates rolling a six-sided die 10 times.
 Print each roll result.
 """
+print("---Task 1---")
 dice = random.randrange(1,7)
+#the range is 1,7 because 1 is inclusive, while 7 is exclusive. If it was 1,6, then it'll only count up to 5 not 6.
 print(dice)
 
 """
@@ -14,15 +16,26 @@ Write a program that generates 5 random floating-point numbers between 0 and 1.
 Then generate 5 random floating-point numbers between 10 and 20.
 Print both sets of numbers.
 """
+print("---Task 2---")
 
+flts = random.uniform(0.0, 1.0)
+#The .uniform code works with the random module by generating random float withing the given range. randrange and randint only works with integers.
+print(flts)
+
+flts2 = random.uniform(10.0, 20.0)
+print(flts2)
 
 """
 Task 3 (random module):
 Create a list of colors: ["red", "blue", "green", "yellow", "purple"].
 Write a program that randomly selects and prints 3 colors from this list without replacement.
 """
+print("---Task 3---")
+
 listy = ["red", "blue", "green", "yellow", "purple"]
 colours = random.sample(listy, 3)
+# The random.sample is basically used to select said items from the list, listy without replacement. Which means that once it's chosen, it won't be picked again.
+#the 3 tells the code to select 3 different colours.
 print(colours)
 
 """
@@ -30,6 +43,10 @@ Task 4 (random module):
 Write a program that creates a list of numbers from 1 to 10, then shuffles the list
 and prints the shuffled result.
 """
+print("---Task 4---")
+
 nums = [1,2,3,4,5,6,7,8,9,10]
+#a list was created for the numbers.
 random.shuffle(nums)
+#the .shuffle code shuffles the numbers in the list into different order.
 print(nums)
