2435c76afd4571a9698d5902056f9d6c4a5d8d06
Mr. Sussy <cogboe@brooklynsteamcenter.org>
2025-10-15 21:12:01 -0400

    dates/dict_done

--- 12_dubious_dates.py
+++ 12_dubious_dates.py
@@ -20,7 +20,7 @@ print("---Task 2---")
 print()
 from datetime import datetime
 current = datetime.now()
-current2 = current.strftime("%m/%d/%Y")
+current2 = current.strftime("%m/%d/%Y")#This code prints out the current time in the date format that you want it in. 
 
 print(current2)
 
@@ -32,14 +32,16 @@ Then find the difference in days between the two.
 print("---Task 3---")
 print()
 check1 = "10/14/2025"
-check2 = "10/17/2025"
+check2 = "10/17/2025"#These two variable stores 2 different dates.
 
 checked1 = datetime.strptime(check1, "%m/%d/%Y")
-
+#The strptime function takes the date from the variable the string is stored in, and converts it to actual date. As long as they are in the same format.
 checked2 = datetime.strptime(check2, "%m/%d/%Y")
+
 print(checked1)
 print(checked2)
 checked3 = checked2 - checked1
+#To check the difference in days between the 2 dates, you apparenlty just put a subtraction sign, and the computer does the rest.
 print(checked3)
 
 """
@@ -49,10 +51,10 @@ age using the datetime module.
 """
 print("---Task 4---")
 print()
-birth = input("When is your birthday to find your current age? (m/d/Y)")
+birth = input("When is your birthday to find your current age? (m/d/Y)")#ask users for their input.
 
-birthed = datetime.strptime(birth, "%m/%d/%Y")
-birthed2 = datetime.now() - birthed
-birthed3 = birthed2.days//365
+birthed = datetime.strptime(birth, "%m/%d/%Y")#This code takes the users birthday input and formats it for the computer to do come actual calculations with.
+birthed2 = datetime.now() - birthed #This code subtracts your birthday from the current day.
+birthed3 = birthed2.days//365 #The code by default tells you the difference in days. So we divide the days by 365 to get the years
 
 print("You are", birthed3, "years old.")
--- 13_contact_dictionary.py
+++ 13_contact_dictionary.py
@@ -1,6 +1,6 @@
 cont_book = {}
 
-while True:
+while True:#Everything is under a While loop so it runs forever instead of rewriting the code.
 
     print("Contact Book Menu:")
     print("1. Add Contact")
@@ -8,34 +8,36 @@ while True:
     print("3. List Contacts")
     print("4. Exit")
 
-    cont = int(input("Enter your choice: "))
-    if cont > 4:
+    cont = int(input("Enter your choice: "))#cont= contact
+    if cont > 4 or cont < 0:#If a user enters a choice more than the given, it loops and asks them to enter a valid choice number.
         print("Please enter a valid choice")
+        print()
     elif cont == 1:
         cont_name = input("Enter contact name: ")
         cont_num = input("Enter the contacts phone number(phone number only): ")
-        if not cont_num.isdigit() or len(cont_num) > 10:
+        if not cont_num.isdigit() or len(cont_num) > 10: #This code basically checks if the user enters an actual number, and if it's less than 10 digits.
+            #it it doesn't come to agreement to the conditional statments, the following is printed.
             print("Enter a number no more than 10 digits.")
         else:
             cont_book[cont_name] = cont_num
-            print("Contact successfully added.")
+            print("Contact successfully added.")#Otherwise, if everything is in agreement with the conditional statement, the contact will be added successfully.
         print()
     elif cont == 2:
         cont_del = input("Which contact would you like to delete? ")
-        if cont_del in cont_book:
-            del cont_book[cont_del]
+        if cont_del in cont_book:#This code checks if the user input is in the list of contacts.
+            del cont_book[cont_del]#It deletes the contact if it's in the list.
             print("Contact successfully deleted.")
         else:
-            print("Contact doesn't exist.")
+            print("Contact doesn't exist.")#If the user input for the contact they want to delete isn't in the contact list, the following is printed.
         print()
     elif cont == 3: 
         print("Contacts:")
         for x in cont_book:
-            print(x, ":", cont_book[x])
+            print(x, ":", cont_book[x])#The for loop code prints out the list of contacts without the dictionary symbols. For example, the curly brackets{}
         print()
     elif cont == 4:
         cont_exit = input("Are you sure?")
         if cont_exit.lower() == "yes":
-            break
+            break#If the user chooses to exit, the code breaks/stops looping.
         print()
 
