be3363e82713b493f9bfbc9b1e464f5a7afa7f7a
Caleb Ogboe <cogboe@brooklynsteamcenter.org>
2025-10-28 10:05:25 -0400

    TrippiTroppi

---
+++ 17_trippy_testing.py
@@ -0,0 +1,96 @@
+"""
+Exercise 1: Divide
+"""
+def divide(a, b):
+  """Divides two numbers, handling potential division by zero.
+
+  Args:
+    a: The numerator.
+    b: The denominator.
+
+  Returns:
+    The quotient, or None if b is zero.
+  """
+
+  if b == 0:
+    return None
+  else:
+    return a / b
+
+assert divide(6,2) == 3#This asserts make sure the function for the division works. It checks the function.
+
+"""
+Exercise 2: Factorial
+"""
+def factorial(n):
+  """Calculates the factorial of a non-negative integer.
+
+  Args:
+    n: A non-negative integer.
+
+  Returns:
+    The factorial of n.
+  """
+
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+assert factorial(4) == 24 #This assert checks if the function for the factorial is workng properly.
+"""
+Exercise 3: String Reverse
+"""
+def reverse_string(string):
+  """Reverses a given string.
+
+  Args:
+    string: A string to be reversed.
+
+  Returns:
+    The reversed string.
+  """
+
+  reversed_string = ""
+  for char in string:
+    reversed_string = char + reversed_string
+  return reversed_string
+assert reverse_string("Money") == "yenoM"#This assert makes sure the reverse_string function is working properly by taking an input.
+"""
+Exercise 4: Fibonacci
+"""
+def fibonacci(n):
+  """Generates the nth Fibonacci number.
+
+  Args:
+    n: The index of the Fibonacci number to calculate.
+
+  Returns:
+    The nth Fibonacci number.
+  """
+
+  if n <= 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+assert fibonacci(5) == 5 #This asserts checks the function for the fibonacci sequence by taking an input and making sure it works.
+
+"""
+Exercise 5: Email Validation
+"""
+
+import re
+
+def is_valid_email(email):
+  """Determines whether email is valid or not
+
+  Args:
+    email: The email to validate
+
+  Returns:
+    Boolean value if email is valid
+  """
+  email_regex = r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+"
+  return re.match(email_regex, email) is not None
+assert is_valid_email("cogboe@brooklynsteamcenter.org")#This assert makes sure that the @ symbol is in the email, verifying that it's an actual email.
